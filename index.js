a544475ea7d018abf1632ea2b18d68c37824a714
Alexandre Cavalheiro S. Tiago Da Silva <alexandre.cssilva@gmail.com>
2019-01-26 17:49:23 -0200

    [~] index -> Minify code.

--- index.js
+++ index.js
@@ -5,34 +5,31 @@ const config = require('./config.json');
 const spawn = require('./schedule.json');
 
 client.on('ready', () => {
-	const checkTime = dateObject => [dateObject.getDay(), dateObject.getHours(), dateObject.getMinutes()];
-
 	/**
 	 * Time in PST (Compared to UTC -5)
 	 */
 	const PST = new Date(Date.now() - 10800000);
-	const currentTime = checkTime(PST);
+	const currentTime = [PST.getDay(), PST.getHours(), PST.getMinutes()];
 
 	/**
 	 * Discord.GuildChannel object.
 	 */
 	const guildChannel = client.guilds.get('420258815112642561').channels.get('532793169461510145');
 
-	/**
-	 * Function to check for the next boss.
-	 */
-	const checkBoss = (day, hour) => spawn[day][hour];
-
 	setInterval(() => {
 		// Check for the next boss.
-		for (let [DD, HH, MM] = currentTime; ; HH++) {
-			if (HH >= 24) HH = '00';
+		for (let [DD, HH, MM] = currentTime; ;) {
+			const hourString = HH.toString().length > 2 ? HH : `0${HH}`;
+			const boss = spawn[DD][`${hourString}:${HH >= 17 ? '15' : '00'}`];
+			if (boss) return guildChannel.send(`${boss} will spawn in ${Math.abs(MM - 60)}`);
+
+			HH += 1;
+			if (HH >= 24) {
+				HH = 0;
+				DD += 1;
+			}
 			if (DD >= 7) DD = 0;
-
-			const boss = checkBoss(DD, `${HH}:${HH >= 17 ? '15' : '00'}`);
-			if (!boss) continue;
-
-			return guildChannel.send(`${boss} will spawn in ${Math.abs(MM - 60)}`);
+			continue;
 		}
 	}, 60000);
 });
